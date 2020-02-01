bde2d16cb5bf5dd6da7b55a5c97b7dbe721400b5
Frank Haugen <frank.haugen@bilagos.no>
2020-02-01 14:35:53 +0100

    Roll-Forward scripts

--- Morphosys/Assets/Scripts/BasicCharacterMovement.cs
+++ Morphosys/Assets/Scripts/BasicCharacterMovement.cs
@@ -3,16 +3,26 @@ using UnityEngine.InputSystem;
 
 public class BasicCharacterMovement : MonoBehaviour
 {
-    private bool IsDKeyDown;
-    private bool IsAKeyDown;
-    private bool IsJumping;
-    private bool IsInAir;
+    [ReadOnly] public bool IsDKeyDown;
+    [ReadOnly] public bool IsAKeyDown;
+    [ReadOnly] public bool IsAttacking;
+    [ReadOnly] public bool IsJumping;
+    [ReadOnly] public bool IsInAir;
+
+    private Rigidbody2D Character;
 
     [Range(1.0f, 10.0f)]
-    public float SpeedMultiplier;
-    void Start()
-    {
+    public float AttackForceMultiplier;
 
+    [Range(1.0f, 10.0f)]
+    public float SpeedForceMultiplier;
+
+    [Range(1.0f, 10.0f)]
+    public float JumpForceMultiplier;
+
+    private void Start()
+    {
+        Character = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -33,7 +43,15 @@ public class BasicCharacterMovement : MonoBehaviour
         {
             IsAKeyDown = true;
         }
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (Mouse.current.leftButton.isPressed && IsInAir && Character.velocity.y < 0)
+        {
+            IsAttacking = true;
+        }
+        if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            IsAttacking = false;
+        }
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && !IsInAir)
         {
             IsJumping = true;
         }
@@ -43,18 +61,25 @@ public class BasicCharacterMovement : MonoBehaviour
     {
         if (IsDKeyDown && !IsInAir)
         {
-            //GetComponent<Rigidbody2D>().AddForce(Vector2.right * 25.0f, ForceMode2D.Force);
-            transform.Translate(Vector2.right);
+            Character.AddForce(Vector2.right * SpeedForceMultiplier, ForceMode2D.Force);
+            //transform.Translate(Vector2.right * Time.deltaTime * SpeedMultiplier);
         }
         if (IsAKeyDown && !IsInAir)
         {
-            //GetComponent<Rigidbody2D>().AddForce(Vector2.left * 25.0f, ForceMode2D.Force);
-            transform.Translate(Vector2.left);
+            Character.AddForce(Vector2.left * SpeedForceMultiplier, ForceMode2D.Force);
+            //transform.Translate(Vector2.left * Time.deltaTime * SpeedMultiplier);
+        }
+        if (IsAttacking)
+        {
+            Character.AddForce(Vector2.down * AttackForceMultiplier, ForceMode2D.Impulse);
+            IsAttacking = false;
+            //transform.Translate(Vector2.left * Time.deltaTime * SpeedMultiplier);
         }
         if (IsJumping && !IsInAir)
         {
-            //GetComponent<Rigidbody2D>().AddForce(Vector2.up * 25.0f, ForceMode2D.Impulse);
-            transform.Translate(Time.deltaTime * 50f * Vector2.up);
+            Character.AddForce(Vector2.up * JumpForceMultiplier, ForceMode2D.Impulse);
+            //transform.Translate(Time.deltaTime * 50f * Vector2.up);
+            //GetComponent<Rigidbody2D>().MovePosition(Vector2.up * Time.deltaTime);
             IsJumping = false;
         }
     }
