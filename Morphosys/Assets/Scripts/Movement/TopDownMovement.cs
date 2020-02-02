47071dfff6452a1a7c26e65be953090f754aa4f9
Frank Haugen <frank.haugen@bilagos.no>
2020-02-02 04:31:12 +0100

    Added topdown

---
+++ Morphosys/Assets/Scripts/Movement/TopDownMovement.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TopDownMovement : MonoBehaviour
+{
+    [ReadOnly] public bool IsDKeyDown;
+    [ReadOnly] public bool IsAKeyDown;
+    [ReadOnly] public bool IsWKeyDown;
+    [ReadOnly] public bool IsSKeyDown;
+
+    [Range(1.0f, 10.0f)] public float TurnMultiplier = 1.0f;
+
+    private Animator animation;
+
+    private void Start()
+    {
+        animation = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.dKey.wasReleasedThisFrame)
+        {
+            IsDKeyDown = false;
+        }
+        if (Keyboard.current.dKey.wasPressedThisFrame)
+        {
+            IsDKeyDown = true;
+        }
+        if (Keyboard.current.aKey.wasReleasedThisFrame)
+        {
+            IsAKeyDown = false;
+        }
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            IsAKeyDown = true;
+        }
+        if (Keyboard.current.wKey.wasReleasedThisFrame)
+        {
+            IsWKeyDown = false;
+        }
+        if (Keyboard.current.wKey.wasPressedThisFrame)
+        {
+            IsWKeyDown = true;
+        }
+        if (Keyboard.current.sKey.wasReleasedThisFrame)
+        {
+            IsSKeyDown = false;
+        }
+        if (Keyboard.current.sKey.wasPressedThisFrame)
+        {
+            IsSKeyDown = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (IsAKeyDown)
+        {
+            transform.Rotate(Vector3.forward * TurnMultiplier);
+        }
+        if (IsDKeyDown)
+        {
+            transform.Rotate(Vector3.back * TurnMultiplier);
+        }
+        if (IsWKeyDown)
+        {
+            transform.Translate(Vector3.right);
+        }
+        if (IsSKeyDown)
+        {
+            transform.Translate(Vector3.left);
+        }
+    }
+}
